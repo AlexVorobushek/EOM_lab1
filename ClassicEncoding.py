96b1f3f321db20e864fb704d76bf2a2b92d640b0
AlexVorobushek <alex080605@mail.ru>
2024-10-06 23:17:01 +0300

    fix classic encoding

--- ClassicEncoding.py
+++ ClassicEncoding.py
@@ -8,17 +8,17 @@ class ClassicEncoding(EncodingMetod):
         self.steps = steps
     
     def encode(self, point: tuple):
-        return sum(
+        return ''.join(
             binary_repr(
-                (point[i]-self.restrictions[i][0])//self.steps[i],
-                ceil(log2((self.restrictions[i][1]-self.restrictions[i][0])//self.steps[i]))
+                ceil((point[i]-self.restrictions[i][0])/self.steps[i]),
+                ceil(log2((self.restrictions[i][1]-self.restrictions[i][0])/self.steps[i]))
             ) for i in range(len(point))
         )
     
     def decode(self, line: str) -> tuple[float, ...]:
         point = []
         for i in range(len(self.steps)):
-            abscissaLineLen = ceil(log2((self.restrictions[i][1]-self.restrictions[i][0])//self.steps[i]))
+            abscissaLineLen = ceil(log2((self.restrictions[i][1]-self.restrictions[i][0])/self.steps[i]))
             abscissaLine, line = line[:abscissaLineLen], line[abscissaLineLen:]
-            point.append(int(abscissaLine, 2))
+            point.append(self.steps[i]*int(abscissaLine, 2)+self.restrictions[i][0])
         return tuple(point)
--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from WeierstrassFunction import WeierstrassFunction
+from ClassicEncoding import ClassicEncoding
 
 if __name__ == "__main__":
-    a, c, eps, N_ITER, N_POP = 1, 1, 1, 12, 12
-    f = WeierstrassFunction()
-    f.draw()
+    N_ITER, N_POP = 12, 12
+
+    targetFunction = WeierstrassFunction()
+    encodingMetod = ClassicEncoding(((-5., 5.), (-5., 5.)), (0.1, 0.1))
+
+    print(encodingMetod.decode(encodingMetod.encode((-0.3, 0.3))))
+
+    # targetFunction.draw()
